3352cbc8055fea54eee0ac602aa4754570a4115a
Thomas McEvoy <tmcevoy213@gmail.com>
2021-07-20 14:26:22 -0400

    fix title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/capital-vol-1/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Marx Notes — Capital Vol.1'
+    }
+  }
 }
